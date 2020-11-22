9a93dea954bcc23a1cbf83c0f4b5caa04ba2f750
Umair Jibran <umairjibran7@gmail.com>
2020-11-22 23:22:27 +0500

    Complete Problem 5

---
+++ p4.js
@@ -0,0 +1,32 @@
+/*
+A Palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+const reverseNumber = number => {
+   let reversedResult = [];
+
+   while (number > 1) {
+      reversedResult.push(Math.trunc(number % 10));
+      number /= 10;
+   }
+
+   return reversedResult.join("");
+};
+
+let palindromeNumbers = [];
+
+for (let i = 100; i < 1000; i++) {
+   for (let j = 100; j < 1000; j++) {
+      let result = "" + i * j;
+      let reversedResult = reverseNumber(result);
+      if (reversedResult === result) {
+         palindromeNumbers = [];
+         palindromeNumbers.push(i);
+         palindromeNumbers.push(j);
+      }
+   }
+}
+
+console.log(palindromeNumbers);
