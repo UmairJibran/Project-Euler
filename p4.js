65b50a4b5e6285e99d29ac57f8fe553d5cadeafa
Umair Jibran <umairjibran7@gmail.com>
2020-11-23 19:37:25 +0500

    Console out the largest number

--- p4.js
+++ p4.js
@@ -4,20 +4,20 @@ A Palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-const { reverseNumber } = require("./util");
+const { reverseNumber, sort } = require("./util");
 
 let palindromeNumbers = [];
 
 for (let i = 100; i < 1000; i++) {
    for (let j = 100; j < 1000; j++) {
-      let result = "" + i * j;
+      let result = i * j;
       let reversedResult = reverseNumber(result);
-      if (reversedResult === result) {
-         palindromeNumbers = [];
-         palindromeNumbers.push(i);
-         palindromeNumbers.push(j);
+      if (reversedResult == result) {
+         palindromeNumbers.push(result);
       }
    }
 }
 
-console.log(palindromeNumbers);
+const sortedArray = sort(palindromeNumbers);
+
+console.log(sortedArray[sortedArray.length - 1]);
