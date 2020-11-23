ced920e6545a13e4458fb574db9ad75f4798f35b
Umair Jibran <umairjibran7@gmail.com>
2020-11-23 19:43:15 +0500

    Console out only the last number

--- p7.js
+++ p7.js
@@ -7,9 +7,12 @@ What is the 10001st prime number?
 const { isPrime } = require("./util");
 
 let total = 0;
+let primeNumbers = [];
 for (let i = 2; total < 10001; i += 1) {
    if (isPrime(i)) {
-      console.log(total + 1, i);
+      primeNumbers.push(i);
       total += 1;
    }
 }
+
+console.log(primeNumbers[total - 1]);
