926ec625628b54919cabfc3117bdcbb9a76b9d22
Umair Jibran <umairjibran7@gmail.com>
2020-11-26 23:55:00 +0500

    Complete Problem 10

---
+++ p10.js
@@ -0,0 +1,15 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+const { isPrime } = require("./util");
+
+let sum = 0;
+
+for (let i = 2; i < 2000000; i++) {
+   if (isPrime(i)) sum += i;
+}
+
+console.log(sum);
